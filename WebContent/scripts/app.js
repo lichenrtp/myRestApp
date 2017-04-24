0019394af5f145201ff515dfa00183900dc6a7a4
lichenrtp <lichenrtp@yahoo.com>
2017-04-24 13:12:52 -0400

    used angular 1.5 framework

--- WebContent/scripts/app.js
+++ WebContent/scripts/app.js
@@ -1,48 +1,61 @@
 "use strict";
 
-function buildJsonStr() {
-	var json = {
-			"firstName": $("#fName").val(),
-			"lastName": $("#lName").val(),
-			"address": $("#address").val(),
-			"company": $("#company").val()	
-	}
-	return JSON.stringify(json);
-}
+//create a module
+var app = angular.module('myRestApp', []);
 
-function save() {
-	var jsonStr = buildJsonStr();
+//add a controller to the module
+app.controller('myCtrl', ['$scope', function($scope) {
+	$scope.firstName = "";
+	$scope.lastName = "";
+	$scope.address = "";
+	$scope.company = "";
 	
-	var xhttp = new XMLHttpRequest();
-	xhttp.onreadystatechange = function() {
-		if (this.readyState == 4 && this.status == 200) {
-			if (this.status == 200) {
-				var result = processData(this.responseText);
-				$("#results").html(result);
-			} else {
-				alert("Something is wrong: " + this.status);
-			}			
-		} 
-	};
-	//xhttp.open("POST", "./mock/save.txt", true);
-	//xhttp.send(jsonStr);
-	xhttp.open("POST", "/myRestApp/rest/cioapi", true);
-	xhttp.send(jsonStr);
-}
-
-function erase() {
-	$("#fName").val("");
-	$("#lName").val("");
-	$("#address").val("");
-	$("#company").val("");
-	$("#results").html("");
-}
-
-function processData(jsonStr) {
-	var jsonObj = JSON.parse(jsonStr);
-	var rtnStr = "First Name: " + jsonObj.firstName[0] + "<br>" +
-						"Last Name: " + jsonObj.lastName[0] + "<br>" +
-						"Address: " + jsonObj.address[0] + "<br>" +
-						"Company: " + jsonObj.company[0] + "<br>";
-	return rtnStr;							
-}
+	//erase 
+  $scope.erase = function() {
+  	$("#fName").val("");
+  	$("#lName").val("");
+  	$("#address").val("");
+  	$("#company").val("");
+  	$("#results").html("");
+  };
+  
+  //create a json string for ajax call
+  $scope.buildJsonStr = function () {
+  	var json = {
+  			"firstName": $("#fName").val(),
+  			"lastName": $("#lName").val(),
+  			"address": $("#address").val(),
+  			"company": $("#company").val()	
+  	}
+  	return JSON.stringify(json);
+  };
+  
+  //call rest api to save data
+  $scope.save = function() {
+  	var jsonStr = $scope.buildJsonStr();
+  	
+  	var xhttp = new XMLHttpRequest();
+  	xhttp.onreadystatechange = function() {
+  		if (this.readyState == 4 && this.status == 200) {
+  			if (this.status == 200) {
+  				$scope.processData(this.responseText);
+  				$scope.$apply();
+  			} else {
+  				alert("Something is wrong: " + this.status);
+  			}			
+  		} 
+  	};
+  	xhttp.open("POST", "/myRestApp/rest/cioapi", true);
+  	xhttp.send(jsonStr);
+  };
+  
+  //update model using the response data
+  $scope.processData = function(jsonStr) {
+  	var jsonObj = JSON.parse(jsonStr);
+  	$scope.firstName = jsonObj.firstName[0];
+  	$scope.lastName = jsonObj.lastName[0];
+  	$scope.address = jsonObj.address[0];
+  	$scope.company = jsonObj.company[0];						
+  };
+  
+}]);
